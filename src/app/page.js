94394f91f96828f2dcff95062000ec7343b50c01
Diaz027 <dami27cito@gmail.com>
2023-12-29 19:29:04 -0300

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,159 @@
-import { Button } from "@nextui-org/button";
+import { Backgraund } from "@/componentes/Backgraund";
 import "./pages.css";
 
 export default function Home() {
   return (
     <>
-      <h1>¡Hola, soy Damian Diaz! 👋</h1>
-      <h2>Desarrollador Full Stack 💻</h2>
-      <br />
-      <p>
-        Apasionado desarrollador Full Stack graduado de "Soy Henry", fusionando
-        creatividad con eficiencia en cada proyecto.
-      </p>
-      <p>
-        Mi enfoque va más allá del código, construyo experiencias que inspiran y
-        resuelven problemas de manera efectiva.
-      </p>
-      <br />
-      <div>
-        <Button className="buttonme">Click me</Button>
+      <div className="container">
+        <div className="header">
+          <div className="h1">
+            <h1>¡Hola, soy Damian Diaz! 👋</h1>
+          </div>
+          <img src="/dami.jpeg" alt="Damian Diaz" className="profile-image" />
+        </div>
+        <div className="h2">
+          <h2>Desarrollador Full Stack 💻</h2>
+        </div>
+        <br />
+        <p>
+          Apasionado desarrollador Full Stack graduado de "Soy Henry", fascinado
+          por la creatividad y la eficiencia en cada proyecto.
+        </p>
+        <p>
+          Mi enfoque va más allá del código, construyo experiencias que inspiran
+          y resuelven problemas de manera efectiva.
+        </p>
+        <br />
+        <div className="h3">
+          <h2>Tecnologias: </h2>
+        </div>
+        <div className="conteinerT">
+          {" "}
+          <a
+            href="https://www.w3schools.com/css/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+              width="40"
+              height="40"
+              className="dark-icon"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html5"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg"
+              alt="nextjs"
+              width="40"
+              height="40"
+              className="dark-icon"
+            />{" "}
+          </a>{" "}
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+              alt="postgresql"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://postman.com" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+              alt="postman"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://reactnative.dev/img/header_logo.svg"
+              alt="reactnative"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://redux.js.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+              alt="redux"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+              alt="tailwind"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+        </div>
+        <Backgraund />
+        <p>hdp joni qliadooooooooo</p>
+        <p>hdp joni qliadooooooooo</p>
+        <p>hdp joni qliadooooooooo</p>
+        <p>hdp joni qliadooooooooo</p>
+        <p>hdp joni qliadooooooooo</p>
       </div>
     </>
   );
---
+++ src/componentes/Backgraund.jsx
@@ -0,0 +1,15 @@
+import "./Backgraund.css";
+
+export const Backgraund = () => {
+  return (
+    <div class="hero">
+      <div class="hero__title">Hello World</div>
+      <div class="cube"></div>
+      <div class="cube"></div>
+      <div class="cube"></div>
+      <div class="cube"></div>
+      <div class="cube"></div>
+      <div class="cube"></div>
+    </div>
+  );
+};
